bd2d7bea4a4923b88078c64804d065d419fe0ec8
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-16 22:22:01 +0200

    Support the config screen

--- src/main/java/bl4ckscor3/mod/theplopper/Configuration.java
+++ src/main/java/bl4ckscor3/mod/theplopper/Configuration.java
@@ -1,8 +1,9 @@
 package bl4ckscor3.mod.theplopper;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
-import org.apache.commons.lang3.tuple.Pair;
 
 public class Configuration {
 	public static final ModConfigSpec CONFIG_SPEC;
@@ -21,13 +22,13 @@ public class Configuration {
 	Configuration(ModConfigSpec.Builder builder) {
 		//@formatter:off
 		displayParticles = builder
-				.comment("Whether particles should show up every time the plopper picks up an item")
+				.comment("Whether particles should show up every time the plopper picks up an item.")
 				.define("displayParticles", true);
 		playSound = builder
-				.comment("Whether a sound should be played when an item gets picked up by the plopper")
+				.comment("Whether a sound should be played when an item gets picked up by the plopper.")
 				.define("playSound", true);
 		bypassOutputSide = builder
-				.comment("If set to true, the plopper will output items to e.g. pipes at all sides instead of just the bottom")
+				.comment("If set to true, the plopper will output items to e.g. pipes at all sides instead of just the bottom.")
 				.define("bypassOutputSide", false);
 		//@formatter:on
 	}
